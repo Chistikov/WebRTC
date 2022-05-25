4dcfe6655fc80bedd4786003ea6806d84a4e4385
VladChistikov <vlad.chistikov998@gmail.com>
2022-05-25 16:31:54 +0300

    Deleted uneccessory code

--- server/server.js
+++ server/server.js
@@ -5,10 +5,6 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', (socket) => {
   console.log('socket connected')
   socket.on('JOIN_ROOM', ({roomId, userId, connectionId}) => {
